603486a87dda27a0d726528d4d45f24641fe5c9c
Mike Jones, II <micujonesii@gmail.com>
2025-04-23 18:57:20 -0400

    Get events from Google Calendar

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -66,8 +66,50 @@ module.exports.getAccessToken = async (event) => {
         });
 };
 
+module.exports.getCalendarEvents = async (event) => {
+    const access_token = decodeURIComponent(
+        `${event.pathParameters.access_token}`
+    );
+    oAuth2Client.setCredentials({ access_token });
+
+    return new Promise((resolve, reject) => {
+        calendar.events.list(
+            {
+                calendarId: CALENDAR_ID,
+                auth: oAuth2Client,
+                timeMin: new Date().toISOString(),
+                singleEvents: true,
+                orderBy: 'startTime',
+            },
+            (error, response) => {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(response);
+            }
+        );
+    })
+        .then((results) => {
+            return {
+                statusCode: 200,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                },
+                body: JSON.stringify({ events: results.data.items }),
+            };
+        })
+        .catch((error) => {
+            return {
+                statusCode: 500,
+                body: JSON.stringify(error),
+            };
+        });
+};
+
 /*
     URL endpoints
     https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url
     https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/token/{code}
+    https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-events/{access_token}
 */
