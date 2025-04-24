f2f92710f04ce2839ac929293500d8f5cd502e62
Mike Jones, II <micujonesii@gmail.com>
2025-04-23 23:54:44 -0400

    First test fails

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './../App';
+
+describe('<App /> component', () => {
+    test('renders list of events', () => {
+        const AppDOM = render(<App />).container.firstChild;
+        expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    });
+});
