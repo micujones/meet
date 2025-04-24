deaea4caf1cd5de7dbe9878f81e43114aba0a8a0
Mike Jones, II <micujonesii@gmail.com>
2025-04-24 12:30:57 -0400

    Render number of events

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -11,4 +11,11 @@ describe('<EventList /> component', () => {
         const EventListComponent = render(<EventList />);
         expect(EventListComponent.queryByRole('list')).toBeInTheDocument();
     });
+
+    test('renders correct number of events', () => {
+        const EventListComponent = render(
+            <EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+        );
+        expect(EventListComponent.getAllByRole('listitem')).toHaveLength(4);
+    });
 });
--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Event } from './Event';
 
-const EventList = () => {
-    return <ul id="event-list"></ul>;
+export const EventList = ({ events }) => {
+    return (
+        <ul id="event-list">
+            {events
+                ? events.map((event) => <Event key={event.id} event={event} />)
+                : null}
+        </ul>
+    );
 };
-
-export default EventList;
