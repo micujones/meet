f2fa697c347b916fbc8e675a694e0d309b557274
Mike Jones, II <micujonesii@gmail.com>
2025-04-25 09:15:03 -0400

    Get events and locations

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import CitySearch from '../components/CitySearch';
+import { getEvents, extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
     let CitySearchComponent;
@@ -31,5 +32,39 @@ describe('<CitySearch /> component', () => {
 
         expect(suggestionsList).toBeInTheDocument();
         expect(suggestionsList).toHaveClass('suggestions');
+    });
+
+    test('updates list of suggestions correctly when user types in city textbox', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+
+        CitySearchComponent.rerender(
+            <CitySearch allLocations={allLocations} />
+        );
+
+        // User types "Berlin" in city textbox
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, 'Berlin');
+
+        // Filter allLocations to locations matching "Berlin"
+        const suggestions = allLocations
+            ? allLocations.filter((location) => {
+                  return (
+                      location
+                          .tpUpperCase()
+                          .indexOf(cityTextBox.value.toUpperCase()) > -1
+                  );
+              })
+            : [];
+
+        // get all <li> elements inside the suggestion list
+        const suggestionsListItems =
+            CitySearchComponent.queryAllByRole('listitem');
+        expect(suggestionsListItems).toHaveLength(suggestions.length + 1);
+
+        for (let i = 0; i < suggestions.length; i += 1) {
+            expect(suggestionsListItems[i].textContent).toBe(suggestions[i]);
+        }
     });
 });
---
+++ src/api.js
@@ -0,0 +1,14 @@
+import mockData from './mock-data';
+
+export const extractLocations = (events) => {
+    const extractedLocations = events.map((event) => {
+        event.location;
+    });
+    // Set removes duplicates
+    const locations = [...new Set(extractedLocations)];
+    return locations;
+};
+
+export const getEvents = async () => {
+    return mockData;
+};
