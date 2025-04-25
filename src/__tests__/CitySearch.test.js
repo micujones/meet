168dfd03956149f10577db27bee5ee57e19eafd8
Mike Jones, II <micujonesii@gmail.com>
2025-04-24 22:57:07 -0400

    Render suggestions list

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -8,11 +8,10 @@ import userEvent from '@testing-library/user-event';
 
 import CitySearch from '../components/CitySearch';
 
-const user = userEvent.setup();
-
-desribe('<CitySearch /> component', () => {
+describe('<CitySearch /> component', () => {
+    let CitySearchComponent;
     beforeEach(() => {
-        const CitySearchComponent = render(<CitySearch />);
+        CitySearchComponent = render(<CitySearch />);
     });
 
     test('renders text input', () => {
@@ -20,5 +19,21 @@ desribe('<CitySearch /> component', () => {
 
         expect(cityTextBox).toBeInTheDocument();
         expect(cityTextBox).toHaveClass('city');
+    });
+
+    test('suggestions list is hidden by default', () => {
+        const suggestionsList = CitySearchComponent.queryByRole('list');
+        expect(suggestionsList).not.toBeInTheDocument();
+    });
+
+    test('renders a list of suggestions when city textbox gains focus', async () => {
+        const user = userEvent.setup();
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+
+        await user.click(cityTextBox);
+        const suggestionsList = CitySearchComponent.queryByRole('list');
+
+        expect(suggestionsList).toBeInTheDocument();
+        expect(suggestionsList).toHaveClass('suggestions');
     });
 });
