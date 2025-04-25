11cbf834206a11092cf0ce6c7453baebe506b0bc
Mike Jones, II <micujonesii@gmail.com>
2025-04-24 20:08:38 -0400

    Add CitySearch component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 
 function App() {
     return (
         <div>
+            <CitySearch />
             <EventList />
         </div>
     );
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,11 +4,16 @@
 
 import { render } from '@testing-library/react';
 import React from 'react';
-import App from './../App';
+import App from '../App';
 
 describe('<App /> component', () => {
     test('renders list of events', () => {
         const AppDOM = render(<App />).container.firstChild;
         expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+    });
+
+    test('renders CitySearch', () => {
+        const AppDOM = render(<App />).container.firstChild;
+        expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
     });
 });
