65434837ad6697c7fbf1d8b1b616dac889c44ac0
Mike Jones, II <micujonesii@gmail.com>
2025-04-24 22:39:42 -0400

    Refractor CitySearchComponent

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -4,11 +4,18 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import CitySearch from '../components/CitySearch';
 
-describe('<CitySearch /> component', () => {
-    test('renders text input', () => {
+const user = userEvent.setup();
+
+desribe('<CitySearch /> component', () => {
+    beforeEach(() => {
         const CitySearchComponent = render(<CitySearch />);
+    });
+
+    test('renders text input', () => {
         const cityTextBox = CitySearchComponent.queryByRole('textbox');
 
         expect(cityTextBox).toBeInTheDocument();
