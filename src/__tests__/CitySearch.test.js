79f113f5ad433715790add0a799ca6589f3a710f
Mike Jones, II <micujonesii@gmail.com>
2025-04-27 01:04:58 -0400

    Click suggestion from list

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -66,4 +66,24 @@ describe('<CitySearch /> component', () => {
             expect(suggestionsListItems[i].textContent).toBe(suggestions[i]);
         }
     });
+
+    test('renders the suggestion text in the textbox upon clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(
+            <CitySearch allLocations={allLocations} />
+        );
+
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, 'Berlin');
+
+        // The suggestion's text content look like this: "Berlin, Germany"
+        const BerlinGermanySuggestion =
+            CitySearchComponent.queryAllByRole('listitem')[0];
+
+        await user.click(BerlinGermanySuggestion);
+
+        expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
 });
--- src/components/CitySearch.jsx
+++ src/components/CitySearch.jsx
@@ -19,6 +19,12 @@ const CitySearch = ({ allLocations }) => {
         setSuggestions(filteredLocations);
     };
 
+    const handleItemClicked = (event) => {
+        const value = event.target.textContent;
+        setQuery(value);
+        setShowSuggestions(false);
+    };
+
     return (
         <div id="city-search">
             <input
@@ -32,9 +38,13 @@ const CitySearch = ({ allLocations }) => {
             {showSuggestions ? (
                 <ul className="suggestions">
                     {suggestions.map((suggestion) => {
-                        return <li key={suggestion}>{suggestion}</li>;
+                        return (
+                            <li key={suggestion} onClick={handleItemClicked}>
+                                {suggestion}
+                            </li>
+                        );
                     })}
-                    <li key="See all cities">
+                    <li key="See all cities" onClick={handleItemClicked}>
                         <b>See all cities</b>
                     </li>
                 </ul>
