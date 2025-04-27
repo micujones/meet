9fcafe5704d3a78a18e783759695249306d07639
Mike Jones, II <micujonesii@gmail.com>
2025-04-27 18:45:39 -0400

    Render event details

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Event from '../components/Event';
 import { getEvents } from '../api';
 
@@ -17,6 +18,7 @@ describe('<Event /> component', () => {
         EventComponent.rerender(<Event event={event} />);
     });
 
+    // Rendering event card
     test('renders event title', async () => {
         expect(EventComponent.queryByText(event.summary)).toBeInTheDocument();
     });
@@ -32,6 +34,67 @@ describe('<Event /> component', () => {
     });
 
     test('render show details button', () => {
-        expect(EventComponent.queryByText('Show Details')).toBeInTheDocument();
+        expect(
+            EventComponent.container.querySelector('#show-details')
+        ).toBeInTheDocument();
+    });
+
+    // Rendering details
+    test("by default, event's details section should be hidden", () => {
+        expect(
+            EventComponent.container.querySelector('#details')
+        ).not.toBeVisible();
+    });
+
+    test("shows the details section when the user clicks on the 'show details' button", async () => {
+        const user = userEvent.setup();
+        const showDetailsButton =
+            EventComponent.container.querySelector('#show-details');
+
+        await user.click(showDetailsButton);
+        const detailsContainer =
+            EventComponent.container.querySelector('#details');
+
+        expect(detailsContainer).toBeVisible();
+    });
+
+    test("hides the 'show details' button after the user clicks it", async () => {
+        const user = userEvent.setup();
+        const showDetailsButton =
+            EventComponent.container.querySelector('#show-details');
+
+        await user.click(showDetailsButton);
+
+        expect(showDetailsButton).not.toBeVisible();
+    });
+
+    test("hides the details section when the user clicks on the 'hide details' button", async () => {
+        const user = userEvent.setup();
+        const showDetailsButton =
+            EventComponent.container.querySelector('#show-details');
+
+        await user.click(showDetailsButton);
+        const detailsContainer =
+            EventComponent.container.querySelector('#details');
+        const hideDetailsButton =
+            EventComponent.container.querySelector('#hide-details');
+
+        await user.click(hideDetailsButton);
+
+        expect(detailsContainer).not.toBeVisible();
+    });
+
+    test("shows the 'show details' button after the user clicks the 'hide details' button", async () => {
+        const user = userEvent.setup();
+        const showDetailsButton =
+            EventComponent.container.querySelector('#show-details');
+
+        await user.click(showDetailsButton);
+        const hideDetailsButton =
+            EventComponent.container.querySelector('#hide-details');
+
+        await user.click(hideDetailsButton);
+
+        expect(showDetailsButton).toBeVisible();
     });
 });
--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Event = ({ event }) => {
+    const [detailsDisplay, setDetailsDisplay] = useState('none');
+
+    const handleDetailsDisplay = () => {
+        const showButton = document.getElementById('show-details');
+
+        if (detailsDisplay === 'none') {
+            showButton.style.display = 'none';
+            setDetailsDisplay('block');
+        } else {
+            showButton.style.display = 'block';
+            setDetailsDisplay('none');
+        }
+    };
+
     return (
         <li>
             {event ? (
                 <>
-                    <h2 id="summary">{event.summary}</h2>
-                    <p id="start-time">{event.start.dateTime}</p>
-                    <p id="location">{event.location}</p>
-                    <button id="show-details">Show Details</button>
+                    <div id="general-info">
+                        <h2 id="summary">{event.summary}</h2>
+                        <p id="start-time">{event.start.dateTime}</p>
+                        <p id="location">{event.location}</p>
+                        <button
+                            id="show-details"
+                            onClick={handleDetailsDisplay}
+                        >
+                            Show Details
+                        </button>
+                    </div>
+                    <div id="details" style={{ display: `${detailsDisplay}` }}>
+                        <h3>About event:</h3>
+                        <p id="description">{event.description}</p>
+                        <button
+                            id="hide-details"
+                            onClick={handleDetailsDisplay}
+                        >
+                            Hide Details
+                        </button>
+                    </div>
                 </>
             ) : null}
         </li>
