dddc518797ff35fc846f45a16d4c36dc4ea1787a
Mike Jones, II <micujonesii@gmail.com>
2025-04-28 17:16:07 -0400

    Renders 32 events when app mounts

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 
+import { getEvents } from './api';
+
 function App() {
+    const [events, setEvents] = useState([]);
+    const [currentNOE, setCurrentNOE] = useState(32); // NOE = number of events
+
+    const fetchData = async () => {
+        const allEvents = await getEvents();
+        setEvents(allEvents.slice(0, currentNOE));
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
         <div>
             <CitySearch />
             <NumberOfEvents />
-            <EventList />
+            <EventList events={events} />
         </div>
     );
 }
