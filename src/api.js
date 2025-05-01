89c1bf9c5e71e7076a7827b11bb691921d7d3695
Mike Jones, II <micujonesii@gmail.com>
2025-04-30 23:29:31 -0400

    Rearrange functions

--- src/api.js
+++ src/api.js
@@ -7,23 +7,55 @@ export const extractLocations = (events) => {
     return locations;
 };
 
+// Fetch a list of events
+export const getEvents = async () => {
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = `https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        if (result) {
+            return result.events;
+        } else {
+            return null;
+        }
+    }
+};
+
+export const getAccessToken = async () => {
+    const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem('access_token');
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get('code');
+
+        if (!code) {
+            const response = await fetch(
+                'https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
+            );
+            const result = await response.json();
+            const { authUrl } = result;
+            return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+    }
+    return accessToken;
+};
+
 const checkToken = async (accessToken) => {
     const response = await fetch(
         `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
     );
     const result = await response.json();
     return result;
-};
-
-const getToken = async (code) => {
-    const encodeCode = encodeURIComponent(code);
-    const response = await fetch(
-        `https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
-    );
-    const { access_token } = await response.json();
-    access_token && localStorage.setItem('access_token', access_token);
-
-    return access_token;
 };
 
 // removes the code from the URL once finished with it
@@ -43,44 +75,13 @@ const removeQuery = () => {
     }
 };
 
-export const getEvents = async () => {
-    if (window.location.href.startsWith('http://localhost')) {
-        return mockData;
-    }
-    const token = await getAccessToken();
+const getToken = async (code) => {
+    const encodeCode = encodeURIComponent(code);
+    const response = await fetch(
+        `https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/token/${encodeCode}`
+    );
+    const { access_token } = await response.json();
+    access_token && localStorage.setItem('access_token', access_token);
 
-    if (token) {
-        removeQuery();
-        const url = `https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-events/${token}`;
-        const response = await fetch(url);
-        const result = await response.json();
-        if (result) {
-            return result.events;
-        } else {
-            return null;
-        }
-    }
-};
-
-export const getAccessToken = async () => {
-    const accessToken = localStorage.getItem('access_token');
-
-    const tokenCheck = accessToken && (await checkToken(accessToken));
-
-    if (!accessToken || tokenCheck.error) {
-        await localStorage.removeItem('access_token');
-        const searchParams = new URLSearchParams(window.location.search);
-        const code = await searchParams.get('code');
-
-        if (!code) {
-            const response = await fetch(
-                'https://mbuu3r70j3.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url'
-            );
-            const result = await response.json();
-            const { authUrl } = result;
-            return (window.location.href = authUrl);
-        }
-        return code && getToken(code);
-    }
-    return accessToken;
+    return access_token;
 };
