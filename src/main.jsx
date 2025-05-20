75dd47ff46f41dfea35e0f93d9ed7c668167cf74
Mike Jones, II <micujonesii@gmail.com>
2025-05-19 23:29:13 -0400

    Import and test Atatus

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import * as atatus from 'atatus-spa';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+atatus.config('ecc523a92aa64efcb39d5800883cf7da').install();
+atatus.notify(new Error('Test Atatus Setup'));
