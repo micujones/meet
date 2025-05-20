9ffe579c177f715e9e122103db2f5cb8e16d85ce
Mike Jones, II <micujonesii@gmail.com>
2025-05-20 18:55:58 -0400

    Create InfoAlert subclass

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -31,3 +31,13 @@ class Alert extends Component {
         );
     }
 }
+
+class InfoAlert extends Alert {
+    constructor(props) {
+        super(props);
+        this.color = 'rgb(0, 0, 255)';
+        this.bgColor = 'rgb(220, 220, 255)';
+    }
+}
+
+export { InfoAlert };
