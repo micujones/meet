88ac3169c7831f47cfcb334b446433edbecd0437
Mike Jones, II <micujonesii@gmail.com>
2025-05-23 09:08:33 -0400

    Init service worker

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import * as atatus from 'atatus-spa';
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
+
+// atatus.config('ecc523a92aa64efcb39d5800883cf7da').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
 );
-
-atatus.config('ecc523a92aa64efcb39d5800883cf7da').install();
+serviceWorkerRegistration.register();
