e8cd45a053fa4c53228c4140e5fe9f03a28bf41b
Mike Jones, II <micujonesii@gmail.com>
2025-05-25 15:39:53 -0400

    Change yellow

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -51,7 +51,7 @@ class ErrorAlert extends Alert {
 class WarningAlert extends Alert {
     constructor(props) {
         super(props);
-        this.color = 'rgb(255, 255, 0)';
+        this.color = 'rgb(250, 220, 10)';
         this.bgColor = 'rgb(255, 255, 224)';
     }
 }
