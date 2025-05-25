06e01b651af2b6253ad3503ebefa036192d7ef83
Mike Jones, II <micujonesii@gmail.com>
2025-05-25 14:48:16 -0400

    Load cached events list

--- src/api.js
+++ src/api.js
@@ -13,6 +13,11 @@ export const getEvents = async () => {
         return mockData;
     }
 
+    // Provide cached events if offline
+    if (!navigator.onLine) {
+        const events = localStorage.getItem('lastEvents');
+        return events ? JSON.parse(events) : [];
+    }
     const token = await getAccessToken();
 
     if (token) {
@@ -21,6 +26,7 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            localStorage.setItem('lastEvents', JSON.stringify(result.events));
             return result.events;
         } else {
             return null;
