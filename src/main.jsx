716b9d36195bb9cd81d6bc843cc70bc81fae9cff
Mike Jones, II <micujonesii@gmail.com>
2025-05-26 16:45:14 -0400

    Track app performance

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// import * as atatus from 'atatus-spa';
+import * as atatus from 'atatus-spa';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
 
-// atatus.config('ecc523a92aa64efcb39d5800883cf7da').install();
+atatus.config('ecc523a92aa64efcb39d5800883cf7da').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
